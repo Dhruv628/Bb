22e0570f4692028a9103f0c6429401723cbbff35
abhay_ratnakar <abhayratnakar03@gmail.com>
2023-12-21 13:52:18 +0530

    "Add speed"

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import connectToDB from './api/Db' 
 import HomePage from "@/app/components/Home/page" 
 import NavBar from './components/Navbar/page'; 
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // import DropboxUpload from './components/Dropbox';
 export default function Home() { 
   return (
 <div className='bg-white h-[100vh]'>
  <HomePage/>
+ <SpeedInsights></SpeedInsights>
 </div>
   )
 }
